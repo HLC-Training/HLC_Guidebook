b997f81b759d931e3a05e62c157093f6e4f59766
HLC-Training <rosenjm4160@gmail.com>
2026-05-27 12:55:28 -0500

    Update app.js

--- app.js
+++ app.js
@@ -3,11 +3,21 @@ fetch('data.json')
     .then(data => {
         document.querySelector('.hero').style.backgroundImage = `url('${data.hero.image}')`;
         document.getElementById('welcome-title').innerText = data.hero.title;
-        const list = document.getElementById('rules-list');
+        
+        const actionGrid = document.getElementById('actions');
+        data.actions.forEach(act => {
+            const btn = document.createElement('a');
+            btn.className = 'btn';
+            btn.href = act.url;
+            btn.innerText = act.label;
+            actionGrid.appendChild(btn);
+        });
+
+        const rulesList = document.getElementById('rules-list');
         data.rules.forEach(rule => {
             const div = document.createElement('div');
             div.className = 'card';
             div.innerText = rule;
-            list.appendChild(div);
+            rulesList.appendChild(div);
         });
     });
